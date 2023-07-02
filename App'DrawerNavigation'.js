140f0ce5b316981bad9582c15dd0a5b86669e2c1
Atiqa119 <110588139+Atiqa119@users.noreply.github.com>
2023-07-02 19:15:38 +0500

    Added Drawer Navigation

--- App'DrawerNavigation'.js
+++ App'DrawerNavigation'.js
@@ -4,11 +4,11 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import {Ionicons} from '@expo/vector-icons';
 import WelcomeScreen from './screens/WelcomeScreen';
 import LoginScreen from './screens/LoginScreen';
-//Add the dependencies of Drawer navigation
-1.npm install @react-navigation/drawer
-2.npx expo install react-native-gesture-handler react-native-reanimated
-Add Reanimated's babel plugin to your babel.config.js
-   3.plugins: ['react-native-reanimated/plugin'],
+//  Add the dependencies of Drawer navigation
+//1.npm install @react-navigation/drawer
+//2.npx expo install react-native-gesture-handler react-native-reanimated
+//  Add Reanimated's babel plugin to your babel.config.js
+// 3.plugins: ['react-native-reanimated/plugin'],
 const Drawer=createDrawerNavigator();
 
 export default function App() {
